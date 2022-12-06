44ecf51ce7a9a44dd6eb045680e932e6fb5a2d37
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-06 23:22:10 +0500

    Update session and registration routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions:       'users/sessions',
+    registrations:  'users/registrations'
+  }
+
   namespace :api do
     namespace :v1 do
       resources :reservations
+      resources :developers
+      resources :members, only: [:index]
+      # devise_for :users, controllers: {
+      #   sessions:       'api/v1/users/sessions',
+      #   registrations:  'api/v1/users/registrations'
+      # }
     end
-  end
-  # resources :developers
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-    namespace :api do
-      namespace :v1 do
-        resources :developers
-      end
-    end  
+  end  
 
   # Defines the root path route ("/")
   # root "articles#index"
