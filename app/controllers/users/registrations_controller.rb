c0b4f92244377bbfe97dfdb0162cc3fbfdbec7a3
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-06 23:21:41 +0500

    Update registration controller to sign_up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, options={})
+    if resource.persisted?
+      render json: {
+        status: { code: 200, message: "Signed Up Successfully", data: resource }
+      }, status: :ok
+    else
+      render json: {
+        status: { message: "User could not be created", errors: resource.errors.full_messages }
+        # status: :unprocessable_entity
+      }
+    end
+  end
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+  
   # GET /resource/sign_up
   # def new
   #   super
