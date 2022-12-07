12c051fbeb11053226bd3a6e4f8d8f6ee0dedb8e
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-08 01:02:20 +0500

    Add password_confirmation in devise parameter sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
   before_action :update_allowed_parameters, if: :devise_controller?
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :email, :password, :current_password)
     end
