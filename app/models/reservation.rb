585133886c0cd4a1386506b05606e37fb7209139
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-13 18:47:06 +0500

    Update validation for reservations model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,6 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :developer
 
-  validates :date, presence: true
+  validates :reservation_date, presence: true
   validates :city, presence: true
 end
