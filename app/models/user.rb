b0412e23fd80e5f6b90b4d2eda835a1d5d0c41f4
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-13 14:53:47 +0500

    Create test cases for user model and update user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
 
   has_many :developers
   has_many :reservations
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
+  validates :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(name: 'john', email: 'john@test.com', password: '1234567')
+  end
+
+  before { subject.save }
+
+  it 'should not be valid with nil name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have valid name' do
+    subject.name = 'john'
+    expect(subject).to be_valid
+  end
+
+  it 'should not be valid with nil email' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have valid email' do
+    subject.email = 'john@test.com'
+    expect(subject).to be_valid
+  end
+
+  it 'should not be valid with nil password' do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have valid password' do
+    subject.password = '1234567'
+    expect(subject).to be_valid
+  end
 end
