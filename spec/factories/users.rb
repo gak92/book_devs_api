dcb826018256e5c9a1c630de0a10195817d6a5e7
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-14 01:56:20 +0500

    Update factory for users and developers

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name { 'John' }
-    email { john@test.com }
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
     password { '1234567' }
     password_confirmation { '1234567' }
   end
