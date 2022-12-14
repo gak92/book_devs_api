adda0ca9683f258849cbb80afc3cfc8b20ca6a31
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-15 03:25:11 +0500

    Fix rubocop error

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  #
+  # rubocop:disable Style/RedundantConstantBase
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
