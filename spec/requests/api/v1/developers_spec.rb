66bd849f45941aaa0493578109ec7bff9bfccf47
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-15 01:12:35 +0500

    Remove unnecessary comments

--- spec/requests/api/v1/developers_spec.rb
+++ spec/requests/api/v1/developers_spec.rb
@@ -45,38 +45,4 @@ RSpec.describe 'Developers', type: :request do
       expect(response.status).to eq(401)
     end
   end
-
-  describe 'when signed in POST /create' do
-    user = User.create(name: 'admin', email: 'admin@test.com', password: '1234567', password_confirmation: '1234567')
-    @user_id = user.id
-    user.admin = true
-    it 'should return 201 (created) status when user sign in' do
-      sign_in user
-      post '/api/v1/developers/', params: {
-        developer: {
-          name: 'Test Dev1',
-          description: '10 Years of experience in development',
-          image: 'Image link',
-          title: 'Back end Developer',
-          salary_exp: 42_000,
-          rating: 5,
-          user_id: @user_id
-        }
-      }
-      expect(response.status).to eq(201)
-    end
-  end
-
-  # describe 'DELETE /destroy' do
-  #   # let(:user) { create(:user) }
-  #   let(:developer) { create(:developer) }
-
-  #   it 'should returns http 200 success' do
-  #     developer.user.admin = true
-  #     sign_in developer.user
-  #     delete "http://127.0.0.1:3000/api/v1/developers/#{developer.id}"
-  #     p response
-  #     expect(response.status).to eq(200)
-  #   end
-  # end
 end
